a50bee609f41ea9bd7db5e7d039381e6e7be4ec4
Jean Fernandine <jfernandine@gmail.com>
2022-04-18 19:35:39 -0300

    testando classe de dominio

---
+++ src/main/java/com/spring/resources/CategoriaResource.java
@@ -0,0 +1,29 @@
+package com.spring.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	public String listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return "REST está funcionando";
+		
+		
+	}
+
+}
